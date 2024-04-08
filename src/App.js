3bab50d91ea0223d6a9c6d2f110787c551295204
sofiabdu <166362782+sofiabdu@users.noreply.github.com>
2024-04-08 10:04:14 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
+
+  const handleClick = () => {
+    toast.success("hello")
+
+  }
   return (
-    <div>App</div>
+    <>
+      <ToastContainer/>
+      <div>App</div>
+      <button onClick={handleClick} className='btn btn-warning btn-lg'>cl</button>
+      </>
+
   )
 }
 
